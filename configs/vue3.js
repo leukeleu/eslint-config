64c708ca8270f31604203067639eee842d5b977a
Tom Westerhuis <twesterhuis@leukeleu.nl>
2023-01-23 10:09:16 +0100

    use require.resolve

--- configs/vue3.js
+++ configs/vue3.js
@@ -1,8 +1,6 @@
-const path = require('path')
-
 module.exports = {
   extends: [
-    path.resolve(__dirname, 'javascript'),
+    require.resolve('./javascript'),
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-recommended',
     'prettier',
