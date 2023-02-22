d5751af71ffca4b78248bd288a76750987d41c1b
Tom Westerhuis <twesterhuis@leukeleu.nl>
2023-02-22 12:09:50 +0100

    Add eslint-plugin-vuejs-accessibility

--- configs/vue2.js
+++ configs/vue2.js
@@ -1,8 +1,10 @@
 module.exports = {
   extends: [
     require.resolve('./javascript'),
+    'plugin:vuejs-accessibility/recommended',
     'plugin:vue/recommended',
     'prettier',
   ],
   parser: 'vue-eslint-parser',
+  plugins: ['vuejs-accessibility'],
 }
